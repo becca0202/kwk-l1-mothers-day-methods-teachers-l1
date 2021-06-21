83d4628dad25b4421749bdee1295a8cd05270045
Rebecca Souza <rebeccacsouza@gmail.com>
2021-06-21 21:08:46 +0000

    Automatically backed up by Learn

---
+++ holiday_greeting.rb
@@ -0,0 +1,6 @@
+def holiday_greeting(holiday = "Mother's Day", name = "Mom", from = "Your Favorite Child")
+  "Happy #{holiday}, #{name}! From, #{from}"
+end
+
+puts holiday_greeting("4th of July", "Beyonce", "Jay-Z")
+puts holiday_greeting
